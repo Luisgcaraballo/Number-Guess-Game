a8a873448cedb7bf7778990f0d7c7547626587c1
Luis G Caraballo <luisgcaraballo@outlook.com>
2018-08-27 16:08:27 -0400

    guessgame.py

---
+++ guessgame.py
@@ -0,0 +1,31 @@
+"""Rolls a pair of dice and asks the user to guess the sum"""
+from random import randint
+from time import sleep
+def get_user_guess():
+  guess = int(raw_input("Guess a number: "))
+  return guess
+
+def roll_dice(number_of_sides):
+  first_roll = randint(1, number_of_sides)
+  second_roll = randint(1, number_of_sides)
+  max_val = number_of_sides * 2
+  print "The maximum possible value is: %d" % max_val
+  guess = get_user_guess()  
+  if guess > max_val:
+     print "Guess cannot be greater than maximum possible value."
+  else:
+      print "Rolling..."
+      sleep(2)
+      print "The first roll is: %d" % first_roll
+      sleep(1)
+      print "The second roll is: %d" % second_roll
+      sleep(1)
+      total_roll = first_roll + second_roll
+      print "The total roll is: %d" % total_roll
+      print "Result..."
+      sleep(1)
+      if guess == total_roll:
+        print "YOU WON"
+      else:
+          print "YOU LOSE"
+roll_dice(6)
